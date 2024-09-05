d93a7f9f83f67372ac5dcef4c2ed42e4d81ef561
Tristan Sandras <tristan@sandras.fr>
2024-09-05 10:46:30 +0200

    new logic to authorize password with @

--- config.py
+++ config.py
@@ -1,30 +1,44 @@
 from dotenv import load_dotenv
 import os
-from urllib.parse import urlparse, quote_plus, urlunparse
+from urllib.parse import quote_plus
 
 load_dotenv()
+
+def has_two_at_symbols(s):
+    """
+    Vérifie s'il y a exactement deux symboles '@' dans la chaîne de caractères.
+
+    :param s: La chaîne de caractères à vérifier.
+    :return: True s'il y a exactement deux '@', False sinon.
+    """
+    return s.count('@') == 2
+
+def split_at_second_at_symbol(s):
+    """
+    Divise la chaîne à la deuxième occurrence de '@'.
+
+    :param s: La chaîne de caractères à diviser.
+    :return: Un tuple contenant deux parties de la chaîne.
+    """
+    parts = s.split('@', 2)
+    if len(parts) > 2:
+        return '@'.join(parts[:2]), parts[2]
+    return s, ''
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     raw_database_url = os.getenv('DATABASE_URL')
 
-    if raw_database_url:
-        parsed_url = urlparse(raw_database_url)
-        username = parsed_url.username
-        password = quote_plus(parsed_url.password)
-        netloc = f"{username}:{password}@{parsed_url.hostname}"
-        if parsed_url.port:
-            netloc += f":{parsed_url.port}"
-        DATABASE_URL = urlunparse((
-            parsed_url.scheme,
-            netloc,
-            parsed_url.path,
-            parsed_url.params,
-            parsed_url.query,
-            parsed_url.fragment
-        ))
+    if raw_database_url and has_two_at_symbols(raw_database_url):
+        scheme, rest = raw_database_url.split('://', 1)
+        username, password_and_host_info = rest.split(':', 1)
+        password, host_info = split_at_second_at_symbol(password_and_host_info)
+        SQLALCHEMY_USER = username
+        SQLALCHEMY_PASSWORD = password
+        SQLALCHEMY_HOST = host_info.split('/')[0]
+        SQLALCHEMY_DB = host_info.split('/')[1]
+        encoded_password = quote_plus(SQLALCHEMY_PASSWORD)
+        SQLALCHEMY_DATABASE_URI = f"{scheme}://{SQLALCHEMY_USER}:{encoded_password}@{SQLALCHEMY_HOST}/{SQLALCHEMY_DB}"
     else:
-        DATABASE_URL = None
-
-    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+        SQLALCHEMY_DATABASE_URI = raw_database_url
