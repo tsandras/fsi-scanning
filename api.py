7d7e7fee71a3edc3f93092c9dcc4e88716e80a82
Tristan Sandras <tristan@sandras.fr>
2024-10-06 09:12:22 +0200

    Improve API and scanner

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 import os
+import csv
 
 from dotenv import load_dotenv
 
@@ -24,6 +25,13 @@ class Product(db.Model):
     barcode = db.Column(db.String(100), nullable=True)
     name = db.Column(db.String(250), nullable=False)
 
+class CompoProduct(db.Model):
+    __tablename__ = 'compoproduct'
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, primary_key=True)
+    barcode = db.Column(db.String(100), nullable=True)
+    quantity = db.Column(db.Integer)
+
 class RepItem(db.Model):
     __tablename__ = 'repitem'
     id = db.Column(db.Integer, primary_key=True)
@@ -32,21 +40,28 @@ class RepItem(db.Model):
     scanned = db.Column(db.Integer, nullable=True)
 
 
-def read_report_preview_lock():
+def read_report_preview_lock(identifier):
     try:
         with open(os.getenv('PATH_TO_LOCK_FILE'), 'r') as file:
-            first_line = file.readline().strip()
-            return first_line if first_line else None
+            reader = csv.reader(file)
+            for row in reader:
+                if row[0] == identifier:
+                    return row[1]  # Retourner le store_id
+        return None
     except FileNotFoundError:
+        print(f"Le fichier n'a pas été trouvé.")
+        return None
+    except Exception as e:
+        print(f"Une erreur s'est produite lors de la lecture du fichier : {e}")
         return None
 
-@app.route('/api/test', methods=['POST'])
-def event_scan():
+@app.route('/api/report_preview', methods=['POST'])
+def report_preview():
     if request.is_json:
         data = request.get_json()
         barcode = data.get('barcode')
-        action = data.get('action')
-
+        identifier = data.get('identifier')
+        print(identifier)
         # updating the report preview
         if read_report_preview_lock():
             store_id = read_report_preview_lock()
@@ -54,19 +69,36 @@ def event_scan():
                 return {'error': 'Store ID is not set in lock file'}, 400
             store_id = int(store_id)
             product = Product.query.filter_by(barcode=barcode).first()
-            if product is None:
+            compo_product = CompoProduct.query.filter_by(barcode=barcode).first()
+            if product is None and compo_product is None:
                 return {'error': 'Product with this barcode not found'}, 404
-            repitem_entry = RepItem.query.filter_by(store_id=store_id, product_id=product.id).first()
+            if product:
+                repitem_entry = RepItem.query.filter_by(store_id=store_id, product_id=product.id).first()
+            else:
+                repitem_entry = RepItem.query.filter_by(store_id=store_id, product_id=compo_product.product_id).first()
             if repitem_entry is not None:
                 if repitem_entry.scanned is None:
                     repitem_entry.scanned = 0
-                repitem_entry.scanned += 1
+                if product:
+                    repitem_entry.scanned += 1
+                else:
+                    repitem_entry.scanned += compo_product.quantity
                 db.session.commit()
 
-        else: # updating product stock
-            new_entry = EventScanning(barcode=barcode, action=action)
-            db.session.add(new_entry)
-            db.session.commit()
+        return {'message': 'Success'}, 200
+    else:
+        return {'error': 'The request payload is not in JSON format'}
+
+@app.route('/api/stock', methods=['POST'])
+def stock():
+    if request.is_json:
+        data = request.get_json()
+        barcode = data.get('barcode')
+        action = data.get('action')
+    
+        new_entry = EventScanning(barcode=barcode, action=action)
+        db.session.add(new_entry)
+        db.session.commit()
 
         return {'message': 'Success'}, 200
     else:
--- scanner.py
+++ scanner.py
@@ -9,8 +9,19 @@ from dotenv import load_dotenv
 load_dotenv()
 
 keys = []
+identifier = None
 
 GTIN_REGEX = r'\b\d{8}(?:\d{4,6})?\b'
+
+
+def flush_input():
+    try:
+        import msvcrt
+        while msvcrt.kbhit():
+            msvcrt.getch()
+    except ImportError:
+        import sys, termios    #for linux/unix
+        termios.tcflush(sys.stdin, termios.TCIOFLUSH)
 
 def on_press(key):
     global keys
@@ -24,7 +35,10 @@ def on_press(key):
         print('special key {0} pressed'.format(key))
 
 def send_post_request(data):
-    url = os.getenv('API_URL')
+    if identifier:
+        url = os.getenv('API_REPORT_PREVIEW_URL')
+    else:
+        url = os.getenv('API_STOCK_URL')
     if url is None:
         print('API_URL is not set')
         logging.error('API_URL is not set')
@@ -56,19 +70,36 @@ def on_release(key):
     if key == Key.enter:
         logging.info('enter released')
         if re.match(GTIN_REGEX, ''.join(keys)):
-            data = {'barcode': ''.join(keys), 'action': 'scanning'}
+            data = {
+                'barcode': ''.join(keys),
+                'action': 'scanning',
+                'identifier': identifier
+            }
             response = send_post_request(data)
             if response:
                 print('Response:', response)
             else:
                 print('Failed to get a response')
         keys = []
+    elif key == Key.esc:
+        keys = []
+        return False    
 
 def keyboard_listener():
+    global identifier
+
     log_file = './keyboard_listener.log'
     logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s - %(message)s')
+    
+    identifier = None  # Réinitialiser identifier si restart_listener est True
+    flush_input()
+    identifier = input("Veuillez entrer votre identifiant (pour faire un rapport de commande) ou appuyez sur entrée (pour un ajout de stock) : ")
+    print(f'Identifiant : {identifier}')
+    
     with Listener(on_press=on_press, on_release=on_release) as listener:
         listener.join()
+
+    keyboard_listener()
 
 if __name__ == "__main__":
     keyboard_listener()
